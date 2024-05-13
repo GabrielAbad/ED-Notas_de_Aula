2953c538029def243402e9762b0655f18eb9b672
GabrielAbad <125308525+GabrielAbad@users.noreply.github.com>
2024-05-13 08:34:55 -0300

    Add files via upload

---
+++ functions.cpp
@@ -0,0 +1,6 @@
+#include "functions.h"
+
+int soma(int iNum1, int iNum2)
+{
+    return iNum1 + iNum2;
+}
---
+++ main (4).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "functions.h"
+
+using namespace std;
+
+int main()
+{
+    
+    cout << soma(13,42) << endl;
+    
+    return 0;
+}
+
